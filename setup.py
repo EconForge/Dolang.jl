35a7cad21f41918ba20a01f9b389d09e34731e21
Pablo Winant <pablo.winant@gmail.com>
2016-10-07 10:10:55 +0100

    Added setup.py file.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='dolang',
       version='0.0.1',
-      py_modules=['dolang'],
+      author=['Pablo Winant'],
+      author_email='pablo.winant@gmail.com',
+      packages=['dolang']
       )
