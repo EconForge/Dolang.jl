693a752b3cc6c09765700fe50619f6cef9912bf6
Pablo Winant <pablo.winant@gmail.com>
2017-05-05 21:06:18 +0100

    PY: update conda recipe.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='dolang',
-      version='0.0.1',
+      version='0.0.4',
       author=['Pablo Winant'],
       author_email='pablo.winant@gmail.com',
       packages=['dolang']
