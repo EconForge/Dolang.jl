ebdca5f83053221b7481ecdefac7c3cf472aa6c2
Spencer Lyon <spencerlyon2@gmail.com>
2017-06-14 10:55:29 -0400

    DOC: after repo change re-add .jl suffix to repo in deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -27,7 +27,7 @@ makedocs(
 )
 
 deploydocs(
-    repo   = "github.com/EconForge/Dolang.git",
+    repo   = "github.com/EconForge/Dolang.jl.git",
     julia  = "0.6",
     target = "build",
     deps   = nothing,
