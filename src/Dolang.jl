8e1f114b1e59ede074e8922973125a5317764958
Pablo Winant <pablo.winant@gmail.com>
2017-10-04 16:14:40 +0200

    Removed Calculus dependency.

--- src/Dolang.jl
+++ src/Dolang.jl
@@ -1,4 +1,4 @@
-__precompile__(false)
+__precompile__(true)
 
 module Dolang
 
@@ -26,29 +26,14 @@ _to_expr(x::Expr) = x
 _to_expr(x::Union{Symbol,Number}) = Expr(:block, x)
 _to_expr(x::AbstractString) = _to_expr(parse(_replace_star_star(x)))
 
-const HAVE_SYMENGINE = try
-    eval(Dolang, :(import SymEngine))
-    true
-catch
-    false
-end::Bool
 
-if HAVE_SYMENGINE
-    import SymEngine
-    deriv(eq::SymEngine.Basic, x) = SymEngine.diff(eq, x)
-    # somebody will probably object to it:
-    deriv(eq::Expr, x::Symbol) = parse(string(SymEngine.diff(SymEngine.Basic(eq), SymEngine.Basic(x))))
-    @inline prep_deriv(eq) = SymEngine.Basic(eq)
-    @inline post_deriv(eq) = SymEngine.walk_expression(eq)
-else
-    import Calculus
-    warn("Using Calculus.jl for symbolic differentiation. This will be slower",
-         " than SymEngine.jl\n. To use SymEngine run the following code: ",
-         "`Pkg.add(\"SymEngine\")`")
-    deriv(eq, x) = Calculus.differentiate(eq, x)
-    @inline prep_deriv(eq) = eq
-    @inline post_deriv(eq) = eq
-end
+import SymEngine
+deriv(eq::SymEngine.Basic, x) = SymEngine.diff(eq, x)
+# somebody will probably object to it:
+deriv(eq::Expr, x::Symbol) = parse(string(SymEngine.diff(SymEngine.Basic(eq), SymEngine.Basic(x))))
+@inline prep_deriv(eq) = SymEngine.Basic(eq)
+@inline post_deriv(eq) = SymEngine.walk_expression(eq)
+
 
 abstract type COrder end
 abstract type FOrder end
