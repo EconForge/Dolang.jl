a5ce9401cdfe18256fee3607635eb4fa0881bb3e
Pablo Winant <pablo.winant@gmail.com>
2018-03-25 16:25:55 +0200

    PY: version bump.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(name='dolang',
-      version='0.0.5',
+      version='0.0.6',
       author=['Pablo Winant'],
       author_email='pablo.winant@gmail.com',
       packages=['dolang'],
