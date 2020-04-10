5de004665b93e5629f096fc19516c57ef24fc09b
Winant Pablo <pablo.winant@gmail.com>
2020-04-10 17:31:59 +0200

    REF: python version moved to dolang.py

--- dolang/__init__.py
+++
@@ -1,3 +0,0 @@
-from dolang.symbolic import parse_string
-from dolang.symbolic import stringify, list_variables, list_symbols, time_shift, steady_state
-from dolang.codegen import to_source
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='dolang',
-      version='0.0.7',
-      author=['Pablo Winant'],
-      author_email='pablo.winant@gmail.com',
-      packages=['dolang'],
-      install_requires=['numpy', 'sympy', 'numba']
-      )
